6b4d8a69d9c2191ab0712f39a099fda91ce82256
RussRussBus <russell@CARLOSs-MacBook-Pro.local>
2025-08-25 01:43:04 -0700

    up

---
+++ russell/template/slurm/jobscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name={rule}
+#SBATCH --account={resources.slurm_account}
+#SBATCH --partition={resources.slurm_partition}
+#SBATCH --output=jobs/%j/{rule}.out
+#SBATCH --err=jobs/%j/{rule}.err
+#SBATCH --cpus-per-task={threads}
+#SBATCH --mem={resources.mem_mb}MB
+#SBATCH --time={resources.time_min}
+
+source /etc/profile
+module load conda
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate <some_env>
+
+{exec_job}
